c5ed0c08887a88b385517af5b515bcd6d7424623
Михаил Ренжин <renzhin@gmail.com>
2023-08-17 08:43:10 +0300

    Проверить редиректы

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -56,3 +56,47 @@ class TestRoutes(TestCase):
                     url = reverse(name, args=(self.notes1.slug,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
+
+    def test_availability_for_notes_list_and_success(self):
+        users_statuses = (
+            (self.author1, HTTPStatus.OK),
+            # (self.author2, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            for name in ('notes:list', 'notes:success'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        for name in (
+            'notes:detail',
+            'notes:edit',
+            'notes:delete',
+            'notes:list',
+        ):
+            # НАДО ПОПРОБОВАТЬ ДОБАВИТЬ УСЛОВИЕ, ЧТО ЕСЛИ СОДЕРЖИТСЯ NOTE, то проверять со слагом.
+            with self.subTest(name=name):
+                if 'list' not in name:
+                    url = reverse(name, args=(self.notes1.slug,))
+                    # Получаем ожидаемый адрес страницы логина, 
+                    # на который будет перенаправлен пользователь.
+                    # Учитываем, что в адресе будет параметр next, в котором передаётся
+                    # адрес страницы, с которой пользователь был переадресован.
+                    redirect_url = f'{login_url}?next={url}'
+                    response = self.client.get(url)
+                    # Проверяем, что редирект приведёт именно на указанную ссылку.
+                    self.assertRedirects(response, redirect_url)
+                else:
+                    url = reverse(name)
+                    redirect_url = f'{login_url}?next={url}'
+                    response = self.client.get(url)
+                    self.assertRedirects(response, redirect_url)
